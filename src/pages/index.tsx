9794b8e786aefbafb0f5ed2dea495396bbecd0f5
Steven Sanséau <steven@sanseau.me>
2020-04-24 18:18:36 +0200

    get initial data on featured post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Home } from 'src/containers/Home'
 import { fetcher } from 'src/utils/Fetcher'
-import { GetPostsQuery, GetSettingsQuery } from 'src/generated/graphql'
+import {
+  GetPostsQuery,
+  GetSettingsQuery,
+  GetFeaturedPostQuery,
+} from 'src/generated/graphql'
 import { getSettings } from 'src/graphql/settings'
-import { getPosts } from 'src/graphql/post'
+import { getPosts, getFeaturedPost } from 'src/graphql/post'
 
 interface PropsIndex {
   initialPostsData?: GetPostsQuery
   initialSettingsData?: GetSettingsQuery
+  initialFeaturedPostData?: GetFeaturedPostQuery
 }
 
 export const getStaticProps = async (): Promise<{ props: PropsIndex }> => {
@@ -15,11 +20,24 @@ export const getStaticProps = async (): Promise<{ props: PropsIndex }> => {
   const initialPostsData: GetPostsQuery = await fetcher(getPosts, {
     after: null,
   })
-  return { props: { initialPostsData, initialSettingsData } }
+  const initialFeaturedPostData: GetFeaturedPostQuery = await fetcher(
+    getFeaturedPost,
+  )
+  return {
+    props: { initialPostsData, initialSettingsData, initialFeaturedPostData },
+  }
 }
 
-const Index: React.FC<PropsIndex> = (props: PropsIndex) => {
-  return <Home initialPostsData={props?.initialPostsData} />
+const Index: React.FC<PropsIndex> = ({
+  initialFeaturedPostData,
+  initialPostsData,
+}: PropsIndex) => {
+  return (
+    <Home
+      initialPostsData={initialPostsData}
+      initialFeaturedPostData={initialFeaturedPostData}
+    />
+  )
 }
 
 export default Index
